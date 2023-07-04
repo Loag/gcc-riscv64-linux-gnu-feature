2f458d7e5a50b4a11e09900302777e924dd9bc92
Logan <10239411+Loag@users.noreply.github.com>
2023-07-04 18:59:51 -0400

    Update install.sh

--- src/gcc-riscv64-linux-gnu/install.sh
+++ src/gcc-riscv64-linux-gnu/install.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -e
 
-which gcc-riscv64-linux-gnu > /dev/null || (apt update && apt install gcc-riscv64-linux-gnu -y -qq)
+which riscv64-linux-gnu-gcc > /dev/null || (apt update && apt install gcc-riscv64-linux-gnu -y -qq)
